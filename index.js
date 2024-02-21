e3747f3f4b07b503568e9ae6b5f7cf447b9d1557
GabrielGMachado445 <gabrielgarcia.2005m@gmail.com>
2024-02-21 16:51:29 -0300

    b

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
-const a =1;
+//const a =1;
 
-function impri eVariavel(){
-    const b = 2;
-    console.log("variável a:" , a);
-    console.log("variável b:" , b);
-}
+//function imprimeVariavel(){
+    //const b = 2;
+    //console.log("variável a:" , a);
+    //console.log("variável b:" , b);
+//}
+
+//imprimeVariavel
+
+    //console.log('variável a' , a)
+    //console.log('variável b' , b)
+
+
+
+    //function calculaArea(altura, largura) {
+        //const area = altura * largura
+        //return area
+    //}
+
+    //Atribui retorno à uma variável
+    //const arecalculada = calculaArea(2, 3)
+
+    // imprime retorno no console
+    //console.log(calculaArea(2, 3))
+
+    //exercicio 3
+
+    //function somaDoisNumeros(numa, numb){
+        //return numa + numb;
+    //}
+    //var resultado = somaDoisNumeros(3, 5);
+    //console.log(resultado);1    
+
+    //exercicio 4
+
+    //function recebeArray(arrayDeNumeros = []){
+        //return[(arrayDeNumeros.length)/2, (arrayDeNumeros
+        //[0])/2];
+        
+    //}
+
+     //console.log(recebeArray([1,2,3,4,5,6,7,8,9]));
+
+     
+     //exercicio 4
+
+     //refaça o exercício 3 com a sintaxe de Arrow Function
+
+     //let somaDoisNumeros = function (numa, numb){
+        //return numa + numb;
+    //}
+    //var resultado = somaDoisNumeros(3, 5);
+    //console.log(resultado);1  
+
+
+
+
