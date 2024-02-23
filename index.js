81ce82c72dc066be19d8eb8f26a5b21ed6a98ee5
GabrielGMachado445 <gabrielgarcia.2005m@gmail.com>
2024-02-23 16:44:08 -0300

    c

--- index.js
+++ index.js
@@ -49,10 +49,126 @@
 
      //let somaDoisNumeros = function (numa, numb){
         //return numa + numb;
+     //}
+     //var resultado = somaDoisNumeros(3, 5);
+     //console.log(resultado);
+
+    
+
+     //aula 7
+     
+      //const professor = {
+       //nome: 'gabriel'  ,
+       //idade: 27 ,
+       //tarefas:['Dar aula' , 'Responder dúvidas'],
+       //contarPiada: function() {
+        //console.log('é pa vê ou pa comê?' )
+       //}
+     //}
+        //console.log(professor.nome)
+        //console.log(professor ["idade"])
+           //professor.nome = "gabriel";
+           //console.log(professor.nome)
+
+     //exercicio 1  
+
+    
+     //var filme = {
+        //nome: "rato" ,
+        //direcao: "luiz" ,
+        //ano:2000 ,
+        //elenco: ["walter" , "joao" ,"davi" , "gabriel"],
+        //visto: true ,
+
+     //}
+
+     //console.log(filme.nome);
+     //console.log(filme.direcao);
+     //console.log(filme["ano"]);
+     //console.log(filme["elenco"]);
+     //console.log(filme.visto);
+     
+
+     //exercicio 2
+
+     //var pessoa ={
+        //nome:"gabriel",
+        //idade:19,
+        //ganeroMusical:"eletronico",
     //}
-    //var resultado = somaDoisNumeros(3, 5);
-    //console.log(resultado);1  
+    
+      //console.log(`o nome da pessoa é ${pessoa.nome},ela tem ${pessoa,idade}, anos e gosta de ${pessoa,generoMusical}`)
 
 
+      //const donoDoPet = {
+        //nome: "vitor hugo" ,
+        //pet: {
+        //nomeDoPet: "wanda" ,
+        //raca: "vira-lata" ,
+        //idaede: 1    
+        //}
+        //}
 
+        //console.log(donoDoPet.pet.nomeDoPet)
+
+        //const curso ={
+            //nome: "Noturno Frontend" ,
+            //linguagens: ["JS" , "CSS" , "HTML"]
+        //}
+
+        //console.log(curso.linguagens[0])
+
+        //const professores = [
+            //{nome: "Andrei", modulo: 1},
+            //{nome: "Vitor", modulo: 2},
+            //{nome: "Mina", modulo: 3}
+            //]
+            
+            //console.log(professores[1].nome)
+
+            //const curso= {
+                //nome: "frontend" ,
+                //linguagens: ["JS" ,"CSS" , "HTML"]
+            //}
+
+            //curso.numeroEstudante = 50
+            //curso['numeroEstudantes'] = 50
+            
+  //var filme = {
+  //nome: "rato" ,
+  //direcao: "luiz" , 
+  //ano:2000 ,
+  //elenco: ["walter" , "joao" ,"davi" , "gabriel"],
+  //visto: true ,
+        
+  //}
+        
+ //filme.personagens = ["personagem 1" , "personagem 2" , "personagem 3" , "personagem 4"];
+             
+ //console.log(filme.elenco[0] +"-" + filme.personagens[0])
+ //console.log(filme.elenco[1] +"-" + filme.personagens[1])
+ //console.log(filme.elenco[2] +"-" + filme.personagens[2])
+ //console.log(filme.elenco[3] +"-" + filme.personagens[3])
+        
+ //filme.elenco[0] = "xuxa";
+ //console.log(filme.elenco[0]);
+ //console.log(filme);       
+ 
+ 
+ //const usuario = {
+    //nome: 'prof' ,
+    //idade: 25,
+    //email: 'prof@senacrs.com.br' ,
+    //cidade: 'são paulo'
+ //}
+
+ const listaDeNomes = ["Mika","Paula","Vitor"]
+
+const copiaListaDeNomes = [...listaDeNomes]
+
+console.log(copiaListaDeNomes) ["Mika","Paula","Vitor"]
+
+          
+          
+  
 
